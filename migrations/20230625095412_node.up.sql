cc7e0bd9d6b7438ff1677736b38495c9da3f853a
tolumide-ng <tolumideshopein@gmail.com>
2023-11-23 23:26:08 +0100

    Removes unused migration files

--- migrations/20230625095412_node.up.sql
+++
@@ -1,7 +0,0 @@
--- Add up migration script here
-CREATE TABLE node(
-    id BIGSERIAL PRIMARY KEY,
-    letter TEXT NOT NULL,
-    count BIGINT NOT NULL DEFAULT 0,
-    terminal BOOLEAN NOT NULL DEFAULT false
-);
--- migrations/20230625095427_node_edge.down.sql
+++
@@ -1,4 +0,0 @@
--- Add down migration script here
-DROP TABLE node_edge;
-DROP FUNCTION scan_rows;
-DROP FUNCTION IF EXISTS allow_first_row_to_be_null;
